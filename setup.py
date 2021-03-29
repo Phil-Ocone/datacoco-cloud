40b6317e822b0b1736e5b1f1a3cc80559fc76d31
Matthew Sarmiento <sarmiento.johnmatt@gmail.com>
2021-03-29 20:18:21 +0800

    Update requires for requests and boto3

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ setup(
     author="Equinox Fitness",
     url="https://github.com/equinoxfitness/datacoco-cloud",
     install_requires=[
-      'requests==2.20.0',
+      'requests==2.25.1',
       'gevent==1.3.4',
-      'boto3>=1.9,<1.11',
+      'boto3>=1.9,<1.16.48',
       'gunicorn==19.9.0',
       'greenlet==0.4.13',
       'future==0.16.0'
