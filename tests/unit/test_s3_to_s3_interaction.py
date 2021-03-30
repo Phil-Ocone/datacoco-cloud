deafb9fed195d40045835ccc336ee079b931cb1c
Matthew Sarmiento <sarmiento.johnmatt@gmail.com>
2021-03-30 19:08:00 +0800

    Formatted s3 to s3 interaction files using black

--- tests/unit/test_s3_to_s3_interaction.py
+++ tests/unit/test_s3_to_s3_interaction.py
@@ -15,48 +15,51 @@ class TestS3ToS3Interaction(unittest.TestCase):
             source_aws_key="",
             source_aws_secret="",
             target_aws_key="",
-            target_aws_secret="") 
+            target_aws_secret="",
+        )
 
     def test_duplicate_objects(self):
-        self.testCls.s3_client_source  = MagicMock()
+        self.testCls.s3_client_source = MagicMock()
         self.testCls.s3_client_target = MagicMock()
         self.testCls.duplicate_objects(
             source_bucket="",
             target_bucket="",
-            source_bucket_prefix = "",
-            target_path="")
+            source_bucket_prefix="",
+            target_path="",
+        )
         self.assertTrue(True)  # Assert that this line is reached without error
 
-
     def test_duplicate_objects_with_suffix(self):
-        self.testCls.s3_client_source  = MagicMock()
+        self.testCls.s3_client_source = MagicMock()
         self.testCls.s3_client_target = MagicMock()
         self.testCls.duplicate_objects(
             source_bucket="",
             target_bucket="",
-            source_bucket_prefix = "",
+            source_bucket_prefix="",
             target_path="",
-            source_bucket_suffix="a")
+            source_bucket_suffix="a",
+        )
         self.assertTrue(True)  # Assert that this line is reached without error
 
     def test_move_objects(self):
-        self.testCls.s3_client_source  = MagicMock()
+        self.testCls.s3_client_source = MagicMock()
         self.testCls.s3_client_target = MagicMock()
         self.testCls.move_objects(
             source_bucket="",
             target_bucket="",
-            source_bucket_prefix = "",
-            target_path="")
+            source_bucket_prefix="",
+            target_path="",
+        )
         self.assertTrue(True)  # Assert that this line is reached without error
 
-
     def test_move_objects_with_suffix(self):
-        self.testCls.s3_client_source  = MagicMock()
+        self.testCls.s3_client_source = MagicMock()
         self.testCls.s3_client_target = MagicMock()
         self.testCls.move_objects(
             source_bucket="",
             target_bucket="",
-            source_bucket_prefix = "",
+            source_bucket_prefix="",
             target_path="",
-            source_bucket_suffix="a")
+            source_bucket_suffix="a",
+        )
         self.assertTrue(True)  # Assert that this line is reached without error
